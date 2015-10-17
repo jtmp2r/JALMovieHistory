3cc77ec4567d5adc2b64f1921f35d8514dc7769c
Lauren Rouse <lauren.l.rouse@gmail.com>
2015-10-17 09:54:19 -0500

    added logic to register.js

--- javascripts/register.js
+++ javascripts/register.js
@@ -9,6 +9,23 @@ define(function(require) {
 
     newUser: function() {
 
+      var ref = new Firebase("https://jal-movie-history.firebaseio.com/");
+      ref.createUser({
+        email     : $("#emailRegister").val,
+        password  : $("passwordRegister").val
+      },function(error, userData) {
+        if (error) {
+          console.log("Error creating user:", error);
+        } else {
+          console.log("Successfully created user account with uid:", userData.uid);
+
+        $("#centerDiv").html(Temp());
+
+        }
+      }); //End userData fxn
+
+
+
     } //End newUser
 
 
